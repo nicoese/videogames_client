8aac48e1eeada403fbb08e76010a0cbd005d6ec3
Nicolas ese <nicolagsalinas2@gmail.com>
2022-09-02 23:52:42 -0300

    prueba sin font

--- src/components/Container.js
+++ src/components/Container.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
 
-  @import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Open+Sans&display=swap');
+  //@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Open+Sans&display=swap');
   
   margin: 0;
   box-sizing: border-box;
@@ -10,5 +10,5 @@ export const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
-  font-family: "JetBrains Mono";
+  //font-family: "JetBrains Mono";
 `
